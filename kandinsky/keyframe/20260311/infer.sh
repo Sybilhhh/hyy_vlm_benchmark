860ccb66c849d4821c84172dc2e4b2dc45b83e74
Sybilhhh <116355384+Sybilhhh@users.noreply.github.com>
2026-03-11 16:54:09 +0800

    Update infer.sh

--- kandinsky/keyframe/20260311/infer.sh
+++ kandinsky/keyframe/20260311/infer.sh
@@ -4,3 +4,5 @@ K5_DEBUG_PROP_KEYFRAME=1 python infer_unified.py \
   --csv_path <含有prop或keyframe任务的CSV> \
   --output_dir <输出目录> \
   --task prop   # 或 --task keyframe，或者CSV里自己有task列
+
+export K5_DEBUG_TRAIN_PROP_KEYFRAME=1
